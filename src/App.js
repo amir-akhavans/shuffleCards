4b79ca0affcf8b3af73ea41ef43fe1b957907fe9
Amir Akhavan <amir.akhavans@gmail.com>
2022-09-01 14:53:51 +0300

    Player mapped to new line

--- src/App.js
+++ src/App.js
@@ -1,62 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 
-function shuffle() {
-  // Arrays of all elements
-  const val1 = ["S", "D", "C", "H"];
-  const val2 = [
-    "A",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10",
-    "J",
-    "Q",
-    "K",
-  ];
-
-  // Cards will be placed in this array
-  let all = [];
-
-  // create a deck of cards
-  for (let i = 0; i < val1.length; i++) {
-    for (let j = 0; j < val2.length; j++) {
-      let card = [`${val2[j]}${val1[i]}`];
-      all.push(card);
-    }
-  }
-  console.log("Player's random cards are:");
-  // console.log("Number of Cards:", all.length);
-  // console.log("All Cards:", all);
-
-  // for (let k = 0; k < 4; k++) {
-  //   console.log(
-  //     `Player ${k}: ${all[Math.floor(Math.random() * all.length)]} - ${
-  //       all[Math.floor(Math.random() * all.length)]
-  //     }`
-  //   );
-  // }
-  //
-  for (let k = 0; k < 4; k++) {
-    // print in console log
-    console.log(
-      `Player ${k}: ${all[Math.floor(Math.random() * all.length)]} - ${
-        all[Math.floor(Math.random() * all.length)]
-      }`
-    );
-    // print in browser
-    const player = `Player ${k}: ${
-      all[Math.floor(Math.random() * all.length)]
-    } - ${all[Math.floor(Math.random() * all.length)]}`;
-  }
-  //
-}
 function App() {
-  //
+  const [play, setPlay] = useState([]);
+
+  const shuffle = () => {
+    // Arrays of all elements
+    const val1 = ["S", "D", "C", "H"];
+    const val2 = [
+      "A",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K",
+    ];
+
+    // Cards will be placed in this array
+    let all = [];
+
+    // create a deck of cards
+    for (let i = 0; i < val1.length; i++) {
+      for (let j = 0; j < val2.length; j++) {
+        let card = [`${val2[j]}${val1[i]}`];
+        all.push(card);
+      }
+    }
+
+    let play = [];
+    for (let k = 0; k < 4; k++) {
+      play[k] = ` Player ${k}: ${
+        all[Math.floor(Math.random() * all.length)]
+      } - ${all[Math.floor(Math.random() * all.length)]}`;
+    }
+    setPlay(play);
+  };
+
   return (
     <div>
       <br />
@@ -68,7 +53,9 @@ function App() {
         Generate Card
       </button>
       <ul>
-        <li>Player:{player}</li>
+        {play.map((player) => (
+          <li>{player}</li>
+        ))}
       </ul>
     </div>
   );
